9738a98bb1d29e0e7431c16352de0c8f34730854
Claire Wilbert <claire.wilbert@gmail.com>
2019-07-01 07:16:57 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,16 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    #find the student
+    @student = Student.find(params[:id])
+    #this toggles the status, if it started as active, it is now inactive(setting it != to itself)
+    @student.active = !@student.active
+    #save the changes to the db
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
---
+++ db/migrate/002_update_students_table.rb
@@ -0,0 +1,5 @@
+class UpdateStudentsTable < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+end
